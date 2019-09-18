701549720c2b2cd5495c49da7d9229a1b161b171
DigitalSnakedotexe <47832927+DigitalSnakedotexe@users.noreply.github.com>
2019-09-18 07:06:40 -0700

    Add files via upload

---
+++ Programas del curso/CineP V1/CineP V1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineP_V1
+{
+    class Pelicula
+    {
+        public string titulo;
+        public Int16 año;
+        public string pais;
+        public string director;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pelicula P1 = new Pelicula();
+            P1.titulo = "La La Land";
+            P1.año = 2016;
+
+            
+            Console.WriteLine("{0}{1}",P1.titulo, P1.año);
+            Console.ReadLine();
+        }
+    }
+}
